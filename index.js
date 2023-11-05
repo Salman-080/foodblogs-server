22fd0df6305d3f364555101720fbbc3e6b5c840a
Md. Salman Siddique <113790578+Salman-080@users.noreply.github.com>
2023-11-05 21:12:01 +0600

    blogs data storing in database and get method for blogsData including sorting and using an specific limit done

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.listen(port, (req,res)=>{
     console.log(`app is running on port: ${port}`)
 })
 
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
 
-
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.pfweqj8.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfweqj8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +30,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const foodBlogsCollection=client.db("FoodBlogsDB").collection("FoodBlogsCollection");
+
+    app.post("/Blogs", async(req,res)=>{
+        const blogDatas= req.body;
+        // console.log(blogDatas)
+    
+        const result=await foodBlogsCollection.insertOne(blogDatas)
+        res.send(result)
+    })
+
+    app.get("/Blogs",async(req,res)=>{
+        const cursor=foodBlogsCollection.find().sort({dateTime: -1}).limit(3);
+        const result= await cursor.toArray();
+        res.send(result)
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -37,7 +54,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
